4ba2dd721461068bd422f4a633fda2314f4f2a4d
Cledianealencar <mariaclediane2512@gmail.com>
2025-05-15 17:04:35 -0300

    Media tres notas atidd

--- src/SomaDoisNumeros.java
+++ src/SomaDoisNumeros.java
@@ -6,8 +6,10 @@ public class SomaDoisNumeros {
 
             System.out.print("digite 1 numero");
             int Num1 = sc.nextInt();
+
             System.out.print("digite outro");
             int Num2 = sc.nextInt();
+
             int Soma = Num1 + Num2;
 
             System.out.println("A soma é "+ Soma);
